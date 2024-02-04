0fa56dbd007d00421dc17fdf47a55fbd148e065c
ghardashpoor-m <mahdi.ghardashpoor@gmail.com>
2024-02-05 02:51:41 +0330

    Add project files.

---
+++ HealthCheckWithUI/Program.cs
@@ -0,0 +1,76 @@
+using HealthChecks.UI.Client;
+
+namespace HealthCheckWithUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHealthChecks()
+                                .AddCheck<SimpleServiceHealthCheck>("Health test of Action 1"
+                                , null, new[] { "myTestTag" });
+            builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+            builder.Services.AddSingleton<SimpleServiceHealthCheck>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            });
+
+            app.MapHealthChecksUI();
+
+            // goto: https://localhost:7025/healthchecks-ui#/healthchecks
+            app.Run();
+        }
+    }
+}
---
+++ HealthCheckWithUI/SimpleServiceHealthCheck.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckWithUI;
+
+public class SimpleServiceHealthCheck : IHealthCheck
+{
+    private readonly Random _random = new();
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        int responseTimeInMS = _random.Next(0, 300);
+        if (responseTimeInMS < 100)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy($"Response time : {responseTimeInMS}"));
+        }
+        else if (responseTimeInMS < 200)
+            return Task.FromResult(HealthCheckResult.Degraded($"Response Time: {responseTimeInMS}"));
+        else
+            return Task.FromResult(HealthCheckResult.Unhealthy($"Response Time: {responseTimeInMS}"));
+    }
+}
